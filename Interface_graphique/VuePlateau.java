164ccfa1b9ba22a38797014d8e3e2e615fb0c29c
baptdes <desnouck.baptiste@gmail.com>
2024-04-24 16:58:59 +0200

    Chevauchement et déplacement des pions OK

--- Interface_graphique/VuePlateau.java
+++ Interface_graphique/VuePlateau.java
@@ -2,23 +2,32 @@ package Interface_graphique;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class VuePlateau extends JPanel {
 
-    public static Font textFont = new Font("Verdana", Font.PLAIN, 12);
-    public static Font titleFont = new Font("Verdana", Font.BOLD, 15);
-    final private static int lengthFrame = 850;
-    final private static int lengthPion = 75;
+    // Constantes publiques pour le style des textes
+    final public static Font textFont = new Font("Verdana", Font.PLAIN, 12);
+    final public static Font titleFont = new Font("Verdana", Font.BOLD, 15);
 
-    public VuePlateau(int[] nbCasesPions, String[] nomsJoueurs) {
+    // Constante
+    final private static int lengthFrame = 850;
+    final public static int lengthPion = 75;
+
+    //Attributs
+    private int[] cases_occupées = new int[40];
+    private ArrayList<?> joueurs;
+    private Pion[] pions;
+
+    public VuePlateau(Pion[] pions, String[] nomsJoueurs) {
+        this.pions = pions;
+
 
         // Chargement de l'image du plateau
         ImageIcon pngPlateau = new ImageIcon("Plateau_monopoly_toulouse.png");
-        ImageIcon pngPion = new ImageIcon("Pion_Monopoly_1.png");
 
         // Redimensionner l'image pour qu'elle prenne toute la hauteur donnée
         Image scaledPlateau = pngPlateau.getImage().getScaledInstance(lengthFrame, lengthFrame, Image.SCALE_SMOOTH);
-        Image scaledPion = pngPion.getImage().getScaledInstance(lengthPion, lengthPion, Image.SCALE_SMOOTH);
 
         // Création d'un JLabel avec l'image redimensionnée
         JLabel plateau = new JLabel(new ImageIcon(scaledPlateau));
@@ -36,11 +45,9 @@ public class VuePlateau extends JPanel {
         plateau.add(bFinPartie);
 
         // Ajout d'un pion
-        for (int i = 0; i < nbCasesPions.length;i++){
-            JLabel pion = new JLabel(new ImageIcon(scaledPion));
-            int[] coord = getCoordonneesCase(nbCasesPions[i]);
-            pion.setBounds(coord[0], coord[1], lengthPion, lengthPion);
-            plateau.add(pion);
+        for (int i = 0; i < pions.length;i++){
+            plateau.add(pions[i]);
+            cases_occupées[pions[i].getPosition()]++;
         }
 
         // Position horizontale initiale pour les joueurs
@@ -60,40 +67,10 @@ public class VuePlateau extends JPanel {
         }
     }
 
-    private int[] getCoordonneesCase(int nbCase){
-        int x = 0; int y = 0;
-        if (nbCase == 0) { //Case de départ
-            x = 735; y = 735;
-        }
-        else if (nbCase <= 9) { //Ligne du bas
-            x = 660 - 69 * (nbCase - 1); y = 750;
-        }
-        else if (nbCase == 10){ //Visite simple
-            x = 20; y = 780;
-        }
-        else if (nbCase <= 19) { //Ligne de gauche
-            x = 10 ; y = 660 - 69 * (nbCase - 11);
-        }
-        else if (nbCase == 20) { //Parking gratuit
-            x = 15; y = 15;
-        }
-        else if (nbCase <= 29) { //Ligne du haut
-            x = 105 + 69 * (nbCase - 21);; y = 10;
-        }
-        else if (nbCase == 30) { //Direction prison
-            x = 750; y = 10;
-        }
-        else if (nbCase <= 39) { //Ligne de droite
-            x = 760; y = 110 + + 69 * (nbCase - 31);
-        }
-        else if (nbCase == 40) { //Case prison
-            x = 35; y = 730;
-        }
-        else{
-            throw new CaseInvalideException();
-        }
-        int[] coordonnees = {x,y};
-        return coordonnees;
+    void deplacerPion(int nbPion,int nbCase){
+        cases_occupées[pions[nbPion].getPosition()]--;
+        pions[nbPion].setPositionPion(nbCase, cases_occupées[nbCase]);
+        cases_occupées[pions[nbPion].getPosition()]++;
     }
 
     public static void main(String[] args) {
@@ -103,8 +80,10 @@ public class VuePlateau extends JPanel {
 
         // Ajout de la VuePlateau à la fenêtre
         String[] nomsJoueurs = {"Pépé","Mémé","Papa","Maman"};
-        int[] cases = {5,10};
-        VuePlateau panel = new VuePlateau(cases,nomsJoueurs);
+        Pion pion1 = new Pion(5,0);
+        Pion pion2 = new Pion(5,1);
+        Pion[] liste_pions = {pion1,pion2};
+        VuePlateau panel = new VuePlateau(liste_pions,nomsJoueurs);
         frame.add(panel);
 
         // Dimension de la fenêtre
