d8870b6e704edcbc7441a72db9064e122a10116a
baptdes <desnouck.baptiste@gmail.com>
2024-05-25 22:22:22 +0200

    Correction Interface_prison

--- Interface_graphique/Interface_prison.java
+++ Interface_graphique/Interface_prison.java
@@ -1,17 +1,17 @@
 package Interface_graphique;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import GestionMonopoly.JoueurMonopoly;
 import GestionMonopoly.Plateau;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Interface_prison extends JFrame {
-	public void action(JoueurMonopoly joueur, Plateau plateau) {
+
+	public Interface_prison(JoueurMonopoly joueur, Plateau plateau) {
+        super("Prison");
         joueur.addToursEnPrison();
         System.out.print("Tour " + joueur.getToursPrison()+ ": ");
 
@@ -20,51 +20,38 @@ public class Interface_prison extends JFrame {
             return;
         }
 
-        JFrame frame = new JFrame("Prison");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 200);
-        frame.setLayout(new GridLayout(0, 1));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300, 200);
+        this.setLayout(new GridLayout(0, 1));
 
         JLabel label = new JLabel(joueur .getNom() + ",vous êtes désormais en Prison.\n");
-        frame.add(label);
+        this.add(label);
         JLabel label1 = new JLabel("Options disponibles :");
-        frame.add(label1);
+        this.add(label1);
         
         ModernButton lancerDoubleButton = new ModernButton("1. Lancer un double", Color.green, Color.white);
-        frame.add(lancerDoubleButton);
+        this.add(lancerDoubleButton);
         
         ModernButton payerAmendeButton = new ModernButton("2. Payer 50$ pour s'échapper",Color.green, Color.white );
-        frame.add(payerAmendeButton);
+        this.add(payerAmendeButton);
         
         ModernButton utiliserCarteButton = null;
         if (joueur.possedeCarteSortiePrison()) {
             utiliserCarteButton = new ModernButton("3. Utiliser carte sortie de prison",Color.green, Color.white );
-            frame.add(utiliserCarteButton);
+            this.add(utiliserCarteButton);
         }
 
         lancerDoubleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Vous lancez les dés ...");
-                plateau.getDes().lancer();
-                System.out.println("Résultat du lancer : " + plateau.getDes().getResultat());
-                if (plateau.getDes().estDouble()) {
-                    System.out.println(joueur.getNom() + " a fait un double !");
-                    sortirJoueurPrison(joueur, plateau);
-                } else {
-                    System.out.println(joueur.getNom() + " n'a pas fait de double !");
-                }
-                frame.dispose();
+                lancerDes(plateau, joueur);
             }
         });
 
         payerAmendeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println(joueur.getNom() + " paye l'amende pour s'échapper de la prison.");
-                joueur.debiter(50); // Supposons que l'amende est de 50$
-                sortirJoueurPrison(joueur, plateau);
-                frame.dispose();
+                payerAmende(plateau,joueur);
             }
         });
 
@@ -72,30 +59,52 @@ public class Interface_prison extends JFrame {
             utiliserCarteButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    System.out.println(joueur.getNom() + " utilise une carte sortie de prison.");
-                    joueur.removeCarteSortiePrison();
-                    sortirJoueurPrison(joueur, plateau);
-                    frame.dispose();
+                    cartePrison(plateau, joueur);
                 }
             });
         }
-
-        frame.setVisible(true);
     }
 
-	 private void sortirJoueurPrison(JoueurMonopoly joueur, Plateau plateau) {
+    private void payerAmende(Plateau plateau, JoueurMonopoly joueur){
+        JOptionPane.showMessageDialog(this, joueur.getNom() + " paye l'amende pour s'échapper de la prison.",
+            "Résultat dés",
+            JOptionPane.INFORMATION_MESSAGE);
+        joueur.debiter(50); // Supposons que l'amende est de 50$
+        sortirJoueurPrison(joueur, plateau);
+    }
+
+    private void cartePrison(Plateau plateau, JoueurMonopoly joueur){
+        JOptionPane.showMessageDialog(this, joueur.getNom() + " utilise une carte sortie de prison.",
+            "Utilisation carte prison",
+            JOptionPane.INFORMATION_MESSAGE);
+        joueur.removeCarteSortiePrison();
+        sortirJoueurPrison(joueur, plateau);
+    }
+
+    private void lancerDes(Plateau plateau, JoueurMonopoly joueur) {
+        System.out.println("Vous lancez les dés ...");
+        plateau.getDes().lancer();
+        JOptionPane.showMessageDialog(this, "Vous avez fait " + plateau.getDes().getDe1() + " et " +  plateau.getDes().getDe1(),
+            "Résultat dés",
+            JOptionPane.INFORMATION_MESSAGE);
+        plateau.attendre(1000);
+        if (plateau.getDes().estDouble()) {
+            JOptionPane.showMessageDialog(this, "Bien joué vous a fait un double !",
+            "Bien joué !",
+            JOptionPane.INFORMATION_MESSAGE);
+            sortirJoueurPrison(joueur, plateau);
+        } else {
+            JOptionPane.showMessageDialog(this, joueur.getNom() + " n'a pas fait un double !",
+            "Dommage",
+            JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+	private void sortirJoueurPrison(JoueurMonopoly joueur, Plateau plateau) {
 	        System.out.println(joueur.getNom() + " sort de prison.");
 	        joueur.setEstEnPrison(false);
 	        joueur.resetToursEnPrison();
 	        plateau.deplacerJoueur(joueur, Plateau.ID_CASE_VISITE_SIMPLE);
-	    }
-	 public static void main(String[] args) {
-	        JoueurMonopoly joueur = new JoueurMonopoly("Hamza", 5, 1500, Color.red);
-	        joueur.addCarteSortiePrison();
-	        ArrayList<JoueurMonopoly> joueurs = new ArrayList<JoueurMonopoly>();
-	        joueurs.add(joueur);
-	        Plateau plateau = new Plateau(joueurs);
-	        new Interface_prison().action(joueur, plateau);
-	    }
+	}
 }
 
